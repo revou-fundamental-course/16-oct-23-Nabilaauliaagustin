e14887412c846a472b9d43dc6dcc7eb594fd43c6
nabilaauliaagustin <nabilaauliaagustin123@gmail.com>
2023-10-26 17:07:24 +0700

    feat: second phase

--- js/script.js
+++ js/script.js
@@ -4,3 +4,26 @@ function replaceName() {
 }
 
 replaceName();
+
+function validateForm() {
+    const name = document.forms["message-form"]["full-name"].value;
+    const birthDate = document.forms["message-form"]["birth-date"].value;
+    const gender = document.forms["message-form"]["gender"].value;
+    const messages = document.forms["message-form"]["messages"].value;
+
+    if (name == "" || birthDate == "" || gender == "" || messages == "") {
+        alert("Mohon isi semua form!");
+        return false;
+    }
+
+    setSenderUI(name, birthDate, gender, messages);
+
+    return false;
+}
+
+function setSenderUI(name, birthDate, gender, messages) {
+    document.getElementById("sender-full-name").innerHTML = name;
+    document.getElementById("sender-birth-date").innerHTML = birthDate;
+    document.getElementById("sender-gender").innerHTML = gender;
+    document.getElementById("sender-messages").innerHTML = messages;
+}
